9ca734045e5e64cfb6dd57f00c61dd7ff0caf130
Marcelo Gomes - Grupo TCS <moreiramarcelo.cientista.cc@gmail.com>
2026-09-02 14:33:17 -0300

    Removendo as imagens dos caminhões do codigo

--- src/data/trucks.ts
+++ src/data/trucks.ts
@@ -1,32 +1,3 @@
-const allImages: Record<string, string> = import.meta.glob(
-  '../assets/images/trucks/**/*.jpg',
-  { eager: true, import: 'default' }
-);
-
-function getImages(folder: string): string[] {
-  return Object.entries(allImages)
-    .filter(([path]) => path.includes(`/${folder}/`))
-    .sort(([a], [b]) => a.localeCompare(b))
-    .map(([, url]) => url);
-}
-
-
-const truckImages = {
-  vwDeliveryExpress: getImages('vwDeliveryExpress'),
-  volvoVM260_2010: getImages('volvoVM260_2010'),
-  vw9150: getImages('vw9.150'),
-  mercedesMB1635: getImages('mercedesMB-1635'),
-  volvoVM260_2011: getImages('volvoVM260_2011'),
-  mercedes1718: getImages('mercedes1718'),
-  fordCargo2429: getImages('fordCargo2429'),
-  vw30280: getImages('vw30-280Prime'),
-  vw24280: getImages('vw24280'),
-  mercedesMB815: getImages('mercedesMB815'),
-  ivecoTector240E22: getImages('ivecoTector240E22'),
-  vw15190: getImages('vw15-190'),
-  mercedesAccelo1017: getImages('mercedesAccelo1017'),
-};
-
 export interface Truck {
   id: number;
   name: string;
